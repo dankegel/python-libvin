dankegel/python-libvin

commit 2281f02f0afa47d1d3fc8063c4af0951c12931a7

Allow 4matic to be spelled 4-M, too

libvin/epa.py
@@ -249,8 +249,7 @@ class EPAVin(Vin):
         '''
         Returns a list of adjectives for this vehicle that might help identify it in EPA model or trim names
         '''
-        # Strongest attribute: the model name!
-        attributes = [self.__remodel()]
+        attributes = []
 
         driveType = self.nhtsa['DriveType']
         if 'AWD' in driveType:
@@ -311,7 +310,7 @@ class EPAVin(Vin):
                 if "1 ton" in s:
                     attributes.append('3500')
             if self.make == 'Mercedes-Benz':
-                if s == 'base-4m': # or s == '4-matic':
+                if s == 'base-4m' or s == 'base 4-m': # or s == '4-matic':
                     # WDC0G4KB8GF033296 base-4m
                     # WDDLJ7GB9EA113284 4-matic
                     # but 1GCEK19B45E223906 gets confused if 4-matic recognized, so oh well for now
@@ -451,6 +450,14 @@ class EPAVin(Vin):
                #    while 'Hybrid' in attributes:
                #        attributes.remove(u'Hybrid')
 
+        # If model name not already in one of the attributes, append it as an attribute
+        # Avoids giving double credit if model name is GLE and there's an attribute GLE550 already
+        m = self.__remodel()
+        for a in attributes:
+            if a.startswith(m):
+                return attributes
+        attributes.append(m)
+
         return attributes
 
     def __get_possible_models(self):
@@ -570,7 +577,7 @@ class EPAVin(Vin):
                 if attrib == "":
                     continue
                 attrib = attrib.upper()
-                #print "Considering attrib %s, uval %s" % (attrib, uval)
+                #print "Considering attrib %s, uval %s; chars_matched was %d" % (attrib, uval, chars_matched)
                 if len(attrib) == 1 and attrib.isdigit():
                     # prevent [2] from matching [2WD], as in Mazda's 2
                     if not re.search('\\b%s\\b' % attrib, uval):
@@ -579,9 +586,13 @@ class EPAVin(Vin):
                     if ("%s-SPD" % attrib) in uval:
                         continue
                 if attrib in uval:
+                    if self.verbosity > 2:
+                        print "matching, len(%s) = %d" % (attrib, len(attrib))
                     # Kludge: give bonus for hybrid match
                     if attrib == "HYBRID":
                         chars_matched += 8
+                        if self.verbosity > 2:
+                            print "8 bonus points for hybrid"
                     if chars_matched == 0:
                         chars_matched = len(attrib)
                     else:
@@ -590,6 +601,8 @@ class EPAVin(Vin):
                 if ((attrib == '2WD' and ('FWD' in uval or 'RWD' in uval)) or
                     (attrib == 'AWD' and '4WD' in uval)):
                       chars_matched += 1
+                      if self.verbosity > 2:
+                          print "1 bonus points for drive type approx match"
 
             # Kludge: give negative bonus for hybrid no-match
             if "HYBRID" in uval and "Hybrid" not in attributes:
@@ -603,6 +616,8 @@ class EPAVin(Vin):
                 best_ids = [key]
                 best_len = len(val)
                 best_matched = chars_matched
+                if self.verbosity > 2:
+                    print "Best so far is %s" % key
             elif (chars_matched > 0 and chars_matched == best_matched):
                 if len(val) < best_len:
                     if self.verbosity > 1:

tests/__init__.py
@@ -1013,6 +1013,18 @@ TEST_DATA = [
      'epa.id' : '37526', 'epa.co2TailpipeGpm': '294.0', 'epa.model' : 'GLE550e 4matic', 'epa.trim' : 'Auto 7-spd, 6 cyl, 3.0 L, Turbo',
     },
 
+    # Breadcrumbs for how libvin/epa.py looks up the epa results:
+    # https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/4JGDA5HB0HA821540
+    # http://www.fueleconomy.gov/ws/rest/vehicle/menu/model?year=2017&make=Mercedes-Benz
+    # http://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=2017&make=Mercedes-Benz&model=GLE350%204matic
+    # http://www.fueleconomy.gov/ws/rest/vehicle/37953
+    {'VIN': '4JGDA5HB0HA821540', 'WMI': '4JG', 'VDS': 'DA5HB0', 'VIS': 'HA821540',
+     'MODEL': 'GLE', 'MAKE': 'Mercedes-Benz', 'YEAR': 2017, 'COUNTRY': 'United States',
+     'REGION': 'north_america', 'SEQUENTIAL_NUMBER': '821540', 'FEWER_THAN_500_PER_YEAR': False,
+     'nhtsa.trim': 'Base 4-M', 'nhtsa.series': 'GLE350',
+     'epa.id' : '37953', 'epa.co2TailpipeGpm': '457.0', 'epa.model' : 'GLE350 4matic', 'epa.trim' : 'Auto 7-spd, 6 cyl, 3.5 L, SIDI',
+    },
+
     # http://www.fueleconomy.gov/ws/rest/vehicle/36406
     {'VIN': '4S3BNAH62G3049699', 'WMI': '4S3', 'VDS': 'BNAH62', 'VIS': 'G3049699',
      'MODEL': 'Legacy', 'MAKE': 'Subaru', 'YEAR': 2016, 'COUNTRY': 'United States',

commit 8c43e28fbda2dc2cf93fdc14f25788c870aa82a9

Fix regressions caused by 2281f02f

libvin/epa.py
@@ -659,7 +659,13 @@ class EPAVin(Vin):
             return None
         if self.verbosity > 0:
             print "Finding model for vin %s" % self.vin
-        ids = self.__fuzzy_match(self.__remodel(), self.__attribs, id2models)
+        m = self.__remodel()
+        ids = self.__fuzzy_match(m, self.__attribs, id2models)
+        if len(ids) == 0 and m not in self.__attribs:
+            # try matching model verbatim even though it's a substring of some attribute;
+            # it helps for the Pontiac 200.
+            self.__attribs.append(m)
+            ids = self.__fuzzy_match(m, self.__attribs, id2models)
         if len(ids) != 1:
             if self.verbosity > 0:
                 print "epa:__get_model: Failed to find model for vin %s" % self.vin

tests/__init__.py
@@ -46,6 +46,18 @@ TEST_DATA = [
      'epa.id' : '16147', 'epa.co2TailpipeGpm': '683.6', 'epa.model' : 'Ram 1500 Pickup 4WD', 'epa.trim' : 'Auto 4-spd, 8 cyl, 5.9 L',
     },
 
+    # Breadcrumbs for how libvin/epa.py looks up the epa results:
+    # https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/1C3CCCBG2FN533158
+    # http://www.fueleconomy.gov/ws/rest/vehicle/menu/model?year=2015&make=Chrysler
+    # http://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=2015&make=Chrysler&model=200
+    # http://www.fueleconomy.gov/ws/rest/vehicle/34831
+    {'VIN': '1C3CCCBG2FN533158', 'WMI': '1C3', 'VDS': 'CCCBG2', 'VIS': 'FN533158',
+     'MODEL': '200', 'MAKE': 'Chrysler', 'YEAR': 2015, 'COUNTRY': 'United States',
+     'REGION': 'north_america', 'SEQUENTIAL_NUMBER': '533158', 'FEWER_THAN_500_PER_YEAR': False,
+     'nhtsa.trim': '200S', 'nhtsa.series': '',
+     'epa.id' : '34831', 'epa.co2TailpipeGpm': '388.0', 'epa.model' : '200', 'epa.trim' : 'Auto 9-spd, 6 cyl, 3.6 L',
+    },
+
     # http://www.vindecoder.net/?vin=1C4RJEAG2EC476429&submit=Decode
     # http://www.fueleconomy.gov/ws/rest/vehicle/33496
     {'VIN': '1C4RJEAG2EC476429', 'WMI': '1C4', 'VDS': 'RJEAG2', 'VIS': 'EC476429',
@@ -1561,6 +1573,18 @@ TEST_DATA = [
     },
 
     # Breadcrumbs for how libvin/epa.py looks up the epa results:
+    # https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/JN8AF5MR4ET454657
+    # http://www.fueleconomy.gov/ws/rest/vehicle/menu/model?year=2014&make=Nissan
+    # http://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=2014&make=Nissan&model=Juke
+    # http://www.fueleconomy.gov/ws/rest/vehicle/34496
+    {'VIN': 'JN8AF5MR4ET454657', 'WMI': 'JN8', 'VDS': 'AF5MR4', 'VIS': 'ET454657',
+     'MODEL': 'Juke', 'MAKE': 'Nissan', 'YEAR': 2014, 'COUNTRY': 'Japan',
+     'REGION': 'asia', 'SEQUENTIAL_NUMBER': '454657', 'FEWER_THAN_500_PER_YEAR': False,
+     'nhtsa.trim': '', 'nhtsa.series': 'Juke/Juke Nismo',
+     'epa.id' : '34496', 'epa.co2TailpipeGpm': '329.0', 'epa.model' : 'Juke', 'epa.trim' : 'Man 6-spd, 4 cyl, 1.6 L, Turbo',
+    },
+
+    # Breadcrumbs for how libvin/epa.py looks up the epa results:
     # https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/JN8AZ2NE0D9060764
     # http://www.fueleconomy.gov/ws/rest/vehicle/menu/model?year=2013&make=Infiniti
     # http://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=2013&make=Infiniti&model=QX56%204WD
@@ -1599,6 +1623,18 @@ TEST_DATA = [
      'REGION': 'asia', 'SEQUENTIAL_NUMBER': '340695', 'FEWER_THAN_500_PER_YEAR': False,
      'nhtsa.trim': 'Base Grade', 'nhtsa.series': 'GRN280L/GRN285L',
      'epa.id' : '36858', 'epa.co2TailpipeGpm': '478.0', 'epa.model' : '4Runner 4WD', 'epa.trim' : 'Auto (S5), 6 cyl, 4.0 L, Part-time 4WD',
+    },
+
+    # Breadcrumbs for how libvin/epa.py looks up the epa results:
+    # https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/JTHBF1D29E5019101
+    # http://www.fueleconomy.gov/ws/rest/vehicle/menu/model?year=2014&make=Lexus
+    # http://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=2014&make=Lexus&model=IS%20F
+    # http://www.fueleconomy.gov/ws/rest/vehicle/34469
+    {'VIN': 'JTHBF1D29E5019101', 'WMI': 'JTH', 'VDS': 'BF1D29', 'VIS': 'E5019101',
+     'MODEL': 'IS', 'MAKE': 'Lexus', 'YEAR': 2014, 'COUNTRY': 'Japan',
+     'REGION': 'asia', 'SEQUENTIAL_NUMBER': '019101', 'FEWER_THAN_500_PER_YEAR': False,
+     'nhtsa.trim': '', 'nhtsa.series': 'GSE30L/GSE31L/GSE35L/GSE36L',
+     'epa.id' : '34469', 'epa.co2TailpipeGpm': '475.0', 'epa.model' : 'IS F', 'epa.trim' : 'Auto (S8), 8 cyl, 5.0 L',
     },
 
     # http://www.vindecoder.net/?vin=JTHBW1GG7D2369737&submit=Decode has no model